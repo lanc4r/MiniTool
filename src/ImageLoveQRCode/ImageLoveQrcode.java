007e4c6f8f036bb5ad8a0054739fc06f871baf5b
lanc4r <lanc4r1996@gmail.com>
2019-05-18 20:59:09 +0800

    增加方法参数 Color，能够指定二维码的颜色

--- src/ImageLoveQRCode/ImageLoveQrcode.java
+++ src/ImageLoveQRCode/ImageLoveQrcode.java
@@ -9,10 +9,15 @@ public class ImageLoveQrcode {
 
     public static void main(String[] args) {
 
-        String content = "https://chllan.cc"; // 二维码中隐藏的数据
+        // 二维码中需要隐藏的地址
+        String content = "https://chllan.cc";
+        // 背景图片地址，目前只支持文件系统读取，后续会支持从网络流中读取
         String bgImgSource = "source.jpg";
+        // 输出的图片地址
         String targetImg = "target.png";
-        createQrcode(bgImgSource, targetImg, content);
+        // Color 类用于指定二维码颜色(R、G、B)
+        Color color = new Color(2, 85, 43);
+        createQrcode(bgImgSource, targetImg, content, color);
         System.out.println("ok~");
     }
 
@@ -24,9 +29,9 @@ public class ImageLoveQrcode {
      * @param content
      *            二维码隐藏的内容
      */
-    public static void createQrcode(String bgImgSource, String targetImg, String content) {
+    public static void createQrcode(String bgImgSource, String targetImg, String content, Color color) {
         try {
-            VisualQRCode.createQRCode(content, bgImgSource, targetImg, 'H', new Color(2, 85, 43), null, null, 449, true,
+            VisualQRCode.createQRCode(content, bgImgSource, targetImg, 'H', color, null, null, 449, true,
                     VisualQRCode.POSITION_DETECTION_SHAPE_MODEL_RECTANGLE, VisualQRCode.FILL_SHAPE_MODEL_CIRCLE);
         } catch (IOException e) {
             e.printStackTrace();
